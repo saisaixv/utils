b4d84d524a3a848f4a3a5b2063ddd14ae4c4c4da
saisai <saisai@localhost.localdomain>
2020-03-12 16:29:36 +0800

    update crypto

--- crypto/crypto.go
+++ crypto/crypto.go
@@ -7,41 +7,53 @@ import (
 	"encoding/base64"
 )
 
-func AesEncrypt(value, key string) string {
-
-	origData := []byte(value)
+func AesEncrypt(orig string, key string) string {
+	// 转成字节数组
+	origData := []byte(orig)
 	k := []byte(key)
-
+	// 分组秘钥
+	// NewCipher该函数限制了输入k的长度必须为16, 24或者32
 	block, _ := aes.NewCipher(k)
+	// 获取秘钥块的长度
 	blockSize := block.BlockSize()
-
+	// 补全码
 	origData = PKCS7Padding(origData, blockSize)
+	// 加密模式
 	blockMode := cipher.NewCBCEncrypter(block, k[:blockSize])
-	crypted := make([]byte, len(origData))
-	blockMode.CryptBlocks(crypted, origData)
-	return base64.StdEncoding.EncodeToString(crypted)
+	// 创建数组
+	cryted := make([]byte, len(origData))
+	// 加密
+	blockMode.CryptBlocks(cryted, origData)
+	return base64.StdEncoding.EncodeToString(cryted)
 }
-
-func AesDecrypt(crypted string, key string) string {
-	cryptedByte, _ := base64.StdEncoding.DecodeString(crypted)
+func AesDecrypt(cryted string, key string) string {
+	// 转成字节数组
+	crytedByte, _ := base64.StdEncoding.DecodeString(cryted)
 	k := []byte(key)
+	// 分组秘钥
 	block, _ := aes.NewCipher(k)
+	// 获取秘钥块的长度
 	blockSize := block.BlockSize()
-	blockMode := cipher.NewCBCDecrypter(block, k[:blocksize])
-	orig := make([]byte, len(cryptedByte))
-
-	blockMode.CryptBlocks(orig, cryptedByte)
-
+	// 加密模式
+	blockMode := cipher.NewCBCDecrypter(block, k[:blockSize])
+	// 创建数组
+	orig := make([]byte, len(crytedByte))
+	// 解密
+	blockMode.CryptBlocks(orig, crytedByte)
+	// 去补全码
 	orig = PKCS7UnPadding(orig)
 	return string(orig)
 }
 
+//补码
+//AES加密数据块分组长度必须为128bit(byte[16])，密钥长度可以是128bit(byte[16])、192bit(byte[24])、256bit(byte[32])中的任意一个。
 func PKCS7Padding(ciphertext []byte, blocksize int) []byte {
 	padding := blocksize - len(ciphertext)%blocksize
 	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
 	return append(ciphertext, padtext...)
 }
 
+//去码
 func PKCS7UnPadding(origData []byte) []byte {
 	length := len(origData)
 	unpadding := int(origData[length-1])
--- main.go
+++ main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 
-	"github.com/saisaixv/utils/redis"
+	"github.com/saisaixv/utils/crypto"
 )
 
 type testTask struct{}
@@ -31,17 +32,41 @@ func main() {
 	// crontab.Start()
 	// select{}
 
-	redis.InitPool("192.168.10.168", 6379, "caton", 1, 30, 30, 30, 30)
+	// redis.InitPool("192.168.10.168", 6379, "caton", 1, 30, 30, 30, 30)
 
-	err := redis.DoStrSet("a", "AAAA")
-	if err != nil {
-		fmt.Printf("set err: %s\n", err.Error())
+	// err := redis.DoStrSet("a", "AAAA")
+	// if err != nil {
+	// 	fmt.Printf("set err: %s\n", err.Error())
+	// }
+
+	// ret, err := redis.DoStrGet("a")
+	// if err != nil {
+	// 	fmt.Printf("get err: %s\n", err.Error())
+	// }
+	// fmt.Println(ret)
+
+	proxy := &Proxy{
+		Host:     "192.168.10.168",
+		Port:     8388,
+		Password: "caton",
+		Method:   "aes-256-cfb",
 	}
 
-	ret, err := redis.DoStrGet("a")
+	bArr, err := json.Marshal(proxy)
 	if err != nil {
-		fmt.Printf("get err: %s\n", err.Error())
+		fmt.Printf("Login:json.Marshal error = %s\n", err.Error())
 	}
+
+	ret := crypto.AesEncrypt(string(bArr), "abcdefghabcdefgh")
 	fmt.Println(ret)
 
+	ret = crypto.AesDecrypt(ret, "abcdefghabcdefgh")
+	fmt.Println(ret)
+}
+
+type Proxy struct {
+	Host     string
+	Port     int
+	Password string
+	Method   string
 }
