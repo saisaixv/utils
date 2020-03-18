dbdfba33cb421a86b17ab462f0726248d93b817c
saisai <saisai@localhost.localdomain>
2020-03-18 18:05:16 +0800

    update db

--- db/db.go
+++ db/db.go
@@ -6,20 +6,16 @@ import (
 	"github.com/jinzhu/gorm"
 )
 
-var Client *DB
-
-type DB struct {
-	Client *gorm.DB
-}
+var DB *gorm.DB
 
 func Release() error {
-	if Client == nil {
+	if DB == nil {
 		return fmt.Errorf("Db CLient is nil")
 	}
 
-	if err := Client.Client.Close(); err != nil {
+	if err := DB.Close(); err != nil {
 		return err
 	}
-
+	DB = nil
 	return nil
 }
