2d8cc666660f3805b4e13d0c37ff997ad71c08b6
saisai <saisai@localhost.localdomain>
2020-03-18 15:44:23 +0800

    update db

--- db/sqlite.go
+++ db/sqlite.go
@@ -1,16 +1,20 @@
 package db
 
 import (
-	"database/sql"
-	"log"
+	// "database/sql"
+	// "log"
 
-	_ "github.com/mattn/go-sqlite3"
+	// _ "github.com/mattn/go-sqlite3"
+	"database/sql"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
 func InitSqlite(path string) *sql.DB {
-	db, err := sql.Open("sqlite3", path)
+	db, err := gorm.Open("sqlite3", path)
 	if err != nil {
-		log.Fatal(err)
+		panic("连接数据库失败")
 	}
 
 	return db
